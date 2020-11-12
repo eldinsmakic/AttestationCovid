330f82c50824c6977f9b70ab07c841241d974dce
Smakic Eldin <eldinsmakic@hotmail.fr>
2020-11-12 21:01:59 +0100

    Initial Commit

---
+++ AttestationCovid/AttestationCovidApp.swift
@@ -0,0 +1,20 @@
+//
+//  AttestationCovidApp.swift
+//  AttestationCovid
+//
+//  Created by eldin smakic on 12/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct AttestationCovidApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
