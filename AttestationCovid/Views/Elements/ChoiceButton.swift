3d03d6e2ef7793c1d7ad2cc24319b1ca218beae5
eldin <eldinsmakic@hotmail.fr>
2020-11-13 22:32:04 +0100

    [Feat]: add new button for selecting MovingMotif

--- AttestationCovid/Views/Elements/ChoiceButton.swift
+++ AttestationCovid/Views/Elements/ChoiceButton.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct ChoiceButton: View {
+    var title: String
+    @State var isChecked = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            isChecked.toggle()
+        }) {
+            HStack {
+                if isChecked {
+                    Rectangle()
+                        .stroke(Color.blue)
+                        .frame(width: 50, height: 50)
+                } else {
+                    Rectangle()
+                        .fill(Color.blue)
+                        .frame(width: 50, height: 50)
+                }
+                Text(title)
+                    .padding()
+            }
+        }
+
     }
 }
 
 struct ChoiceButton_Previews: PreviewProvider {
     static var previews: some View {
-        ChoiceButton()
+        ChoiceButton(title: "activité professionnelle")
     }
 }
