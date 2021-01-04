94569d158c37be578d1895ae51caf0330ec86671
Smakic Eldin <eldinsmakic@hotmail.fr>
2021-01-04 02:08:13 +0100

    [Fix]: add better scale for big image

--- AttestationCovid/App/Utils/qrcodeUtils.swift
+++ AttestationCovid/App/Utils/qrcodeUtils.swift
@@ -15,7 +15,8 @@ func generateQRCode(from string: String) -> UIImage {
     let data = Data(string.utf8)
     filter.setValue(data, forKey: "inputMessage")
 
-    if let outputImage = filter.outputImage {
+    let transform = CGAffineTransform(scaleX: 10, y: 10)
+    if let outputImage = filter.outputImage?.transformed(by: transform) {
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             return UIImage(cgImage: cgimg)
         }
