e200327a33023d0b0cac2c988f8bbbc97ae71063
Smakic Eldin <eldinsmakic@hotmail.fr>
2021-01-05 01:55:49 +0100

    [Fix]: for now set simple rule for manView routing

--- AttestationCovid/App/Views/MainView.swift
+++ AttestationCovid/App/Views/MainView.swift
@@ -10,22 +10,33 @@ import SwiftUI
 struct MainView: View {
     @EnvironmentObject var appRouting: AppRouting
     @EnvironmentObject var profilLocalData: ProfilLocalData
+    
     var body: some View {
-        TabView {
+        TabView(selection: $appRouting.router) {
+//            PdfView()
             MovingMotifFormView()
                 .environmentObject(appRouting)
                 .environmentObject(profilLocalData)
                 .tabItem {
-                    Image(systemName: appRouting.router == Router.main  ? "house.fill" : "house")
+//                    Image(systemName: appRouting.router == Router.main  ? "house.fill" : "house")
+                    Image(systemName: "house")
                     Text("Home")
-                }
+                }.tag(Router.main)
+
             ListProfilsView()
                 .environmentObject(appRouting)
                 .environmentObject(profilLocalData)
                 .tabItem {
-                    Image(systemName: appRouting.router == Router.profile ? "person.fill" : "person")
+//                    Image(systemName: appRouting.router == Router.profile ? "person.fill" : "person")
+                    Image(systemName: "person")
                     Text("Profiles")
-                }
+                }.tag(Router.profile)
+
+            ListPDFView()
+                .tabItem {
+                    Image(systemName: "doc")
+                    Text("Attestations")
+                }.tag(Router.attestationPDFs)
         }
     }
 }
