f2e4fc51499e6357f1bcad204c5f617c203f67e4
Smakic Eldin <eldinsmakic@hotmail.fr>
2021-01-07 21:40:39 +0100

    [Feat]: add Equatable protocol to Profil

--- AttestationCovid/App/Models/Profil.swift
+++ AttestationCovid/App/Models/Profil.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Defaults
 
-public struct Profil: Codable, Identifiable {
+public struct Profil: Codable, Identifiable, Equatable {
     public var id = UUID()
     var firstName: String
     var lastName: String
