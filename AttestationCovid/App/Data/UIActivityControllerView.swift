cc3392e300689f550db387188dbdc0fcc7fe9071
Smakic Eldin <eldinsmakic@hotmail.fr>
2021-01-10 20:44:31 +0100

    [Feat]: Add ActivityViewControleur to share and save pdf

---
+++ AttestationCovid/App/Data/UIActivityControllerView.swift
@@ -0,0 +1,23 @@
+//
+//  UIActivityControllerView.swift
+//  AttestationCovid
+//
+//  Created by eldin smakic on 10/01/2021.
+//
+
+import UIKit
+import SwiftUI
+
+struct ActivityViewController: UIViewControllerRepresentable {
+
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityViewController>) -> UIActivityViewController {
+        let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: UIViewControllerRepresentableContext<ActivityViewController>) {}
+
+}
