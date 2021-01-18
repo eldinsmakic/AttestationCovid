ff2a0c4ba2af71931c4893495f95ca4ac6952823
Smakic Eldin <eldinsmakic@hotmail.fr>
2021-01-18 22:06:12 +0100

    [Fix]: Setup PDFView, remove all useless things

--- AttestationCovid/App/Views/PdfView.swift
+++ AttestationCovid/App/Views/PdfView.swift
@@ -10,29 +10,22 @@ import SwiftUI
 import PDFKit
 import UIKit
 
-let url = Bundle.main.url(forResource: "attestation", withExtension: "pdf", subdirectory: nil, localization: nil)
-
-
 struct PdfViewToSwifUI: UIViewRepresentable {
     typealias UIViewType = PDFView
-        var url: URL?
 
-        init(url : URL?) {
+        let url: URL
+
+        init(url : URL) {
             self.url = url
         }
 
         func makeUIView(context: Context) -> PDFView {
            let pdfView = PDFView()
 
-           if let url = url {
+            pdfView.document = PDFDocument(url: url)
+            pdfView.displayMode = .singlePageContinuous
 
-              pdfView.document = PDFDocument(url: url)
-              pdfView.displayMode = .singlePage
-
-              pdfView.autoScales = true
-              test()
-           }
-
+            pdfView.autoScales = true
 
            return pdfView
        }
@@ -43,7 +36,7 @@ struct PdfViewToSwifUI: UIViewRepresentable {
 }
 
 struct PdfView: View {
-
+    let url = URL(string: "helo")!
     var body: some View {
         VStack {
             PdfViewToSwifUI(url: url)
@@ -51,68 +44,3 @@ struct PdfView: View {
         }
     }
 }
-
-func test() {
-    let pdf = PDFDocument(url: url!)
-
-
-    print("test")
-
-    if let pdf = pdf {
-        drawText(text: "01/02/1994", x: 119, y: 645, font: font, to: pdf)
-//      
-        print("drawned")
-        print(pdf.allowsDocumentChanges)
-        let annotation = PDFAnnotation(bounds: CGRect(x: 100, y: 100, width: 100, height: 100), forType: .freeText, withProperties: nil)
-       annotation.contents = "Hello, world!"
-        annotation.font = UIFont.systemFont(ofSize: 40.0)
-       annotation.fontColor = .blue
-       annotation.color = .clear
-       pdf.page(at: 0)!.addAnnotation(annotation)
-        let page = PDFPage()
-        page.addAnnotation(annotation)
-        print(pdf.page(at: 0)?.annotations)
-        pdf.insert(page, at: 1)
-
-        if let data = pdf.dataRepresentation() {
-          try? data.write(to: url!)
-            print("saving")
-        }
-    }
-//        do {
-//            if let data = pdf.dataRepresentation() {
-//
-//                var url = pdf.documentURL?.deletingLastPathComponent()
-//                url = url?.appendingPathComponent("attes2.pdf")
-//
-//                let fileManager = FileManager.default
-//                let urls = fileManager.urls(for: .userDirectory, in: .userDomainMask)
-//                    if let applicationSupportURL = urls.last {
-//                        do{
-//                            try fileManager.createDirectory(at: applicationSupportURL, withIntermediateDirectories: true, attributes: nil)
-//                            let testUrl = applicationSupportURL
-//                            testUrl.appendingPathComponent("attes2.pdf")
-//                            print(testUrl.absoluteURL)
-//                            try data.write(to: testUrl, options: [.noFileProtection, .atomicWrite])
-//                        }
-//                        catch{
-//                            print(error)
-//                        }
-//                    }
-//
-//
-////                try data.write(to: pdf.documentURL!.absoluteURL)
-//
-//
-//                print("Write to fill" )
-//            }
-//            print("hh")
-//        } catch let error {
-//            print(error)
-//        }
-//    }
-
-}
-
-
-
