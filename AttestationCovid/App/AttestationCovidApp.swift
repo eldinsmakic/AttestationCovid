d06a886f43fc60a1b4514c90a3dfa8019c4513a1
Smakic Eldin <eldinsmakic@hotmail.fr>
2021-01-20 02:25:30 +0100

    [Feat]: Add Base Store AppStore

--- AttestationCovid/App/AttestationCovidApp.swift
+++ AttestationCovid/App/AttestationCovidApp.swift
@@ -6,13 +6,39 @@
 //
  
 import SwiftUI
+import ComposableArchitecture
+
+struct AppState {
+    var routerState: RouterState
+    var raisonState: RaisonState
+}
+
+enum AppAction {
+    case router(RouterAction)
+    case raison(RaisonAction)
+}
+
+let appReducer = Reducer<AppState, AppAction, Void>.combine(Reducer<AppState, AppAction,Void> {
+    _, _, _ in
+    return .none
+    },
+    routerReducer.pullback(
+        state: \.routerState,
+        action: /AppAction.router,
+        environment: {}
+    ),
+    raisonReducer.pullback(
+        state: \.raisonState,
+        action: /AppAction.raison,
+        environment: {}
+    )
+)
 
 @main
 struct AttestationCovidApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView(store: .init(initialState: .init(), reducer: appReducer, environment: ()))
-                .environmentObject(AppRouting())
+            MainView(store: .init(initialState: .init(routerState: .init(), raisonState: .init()), reducer: appReducer, environment: ()))
         }
     }
 }
